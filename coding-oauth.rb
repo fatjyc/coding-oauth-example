7ea22461fd757d33c6fc80f989a1d1967e6686ee
jiong <fatjyc@gmail.com>
2017-05-26 21:24:04 +0800

    modified:   coding-oauth.rb 	new file:   views/index.erb 	modified:   views/oauth.erb

--- coding-oauth.rb
+++ coding-oauth.rb
@@ -21,19 +21,26 @@ class CodingOauth < Sinatra::Base
   end
 
   get '/' do
-    '<a href="' + @@url  + '/oauth_authorize.html?client_id=' + @@client_id + '&redirect_uri=' + @@host  + '/api/oauth/callback&response_type=code&scope=all">go</a>'
+    @url = "#{@@url}/oauth_authorize.html?client_id=#{@@client_id}&redirect_uri=#{@@host}/api/oauth/callback&response_type=code&scope=all"
+    erb :index
   end
 
   get '/api/oauth/callback' do
     code = params[:code]
-    @url = @@url + '/api/oauth/access_token?client_id='+@@client_id+'&client_secret=' + @@client_secret + '&grant_type=authorization_code&code=' + code
+    team = params[:team]
+    domain = "https://#{team}.coding.net"
+    @url = "#{domain}/api/oauth/access_token?client_id=#{@@client_id}&client_secret=#{@@client_secret}&grant_type=authorization_code&code=#{code}&team=#{team}"
     uri = URI.parse(@url)
     respone = Net::HTTP.get(uri)
     respone = JSON.parse(respone)
     access_token = respone['access_token']
-    uri = URI.parse(@@url + '/api/current_user?access_token=' + access_token)
+    uri = URI.parse("#{domain}/api/current_user?access_token=#{access_token}")
     respone = Net::HTTP.get(uri)
     @current_user = JSON.parse(respone)
+    if @current_user['code'] != 0
+      redirect '/'
+      return
+    end
     @current_user = @current_user['data']
     erb :oauth
   end
